df3626b00cefff8c429cd75dd86f610dd86c7b75
Martin Taibr <taibr.martin@gmail.com>
2020-07-04 16:52:26 +0200

    load textures and maps

---
+++ src/README.md
@@ -0,0 +1,30 @@
+Recreational Warfare .rs
+========================
+
+A top-down vehicle shooter based on an old Windows game called RecWar.
+
+TODO screenshot
+TODO github description, tags
+
+The Original Game
+-----------------
+
+Windows .exe only but runs ok-ish wine (sometimes freezes on map load). Includes a map editor.
+
+homepage: http://recreationalwarfare.atspace.com/index_willem.html (the game's download is broken but still hosts extra maps)
+unofficial homepage: http://www.recwar.50webs.com/
+archive.org download: https://archive.org/details/recwar_201903
+archive.org download with extra maps: https://archive.org/details/RecWar
+
+The binaries in both archive.org links are identical to what i got on an old CD so should be safe.
+
+TODO look at:
+https://hupage.mypage.cz/menu/domovska-stranka/download/recwar
+https://recwar.osoba.cz/rubriky/download
+
+Licence
+-------
+
+All code is available under GPL-v3 or newer. TODO include, make sure GH shows it
+
+All assets (maps, textures, sounds, etc.) are taken from the original RecWar by Willem Janssen.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use std::fs;
+
+#[derive(Debug, Clone)]
+struct Texture {
+    name: String,
+    a: i32,
+    b: f32,
+    c: f32,
+}
+
+impl Texture {
+    fn new(name: String, a: i32, b: f32, c: f32) -> Self {
+        Self { name, a, b, c }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let textures = load_textures();
+    dbg!(&textures);
+    let map = load_map("maps/Corners (4).map");
+    dbg!(&map);
+}
+
+fn load_map(path: &str) -> Vec<Vec<usize>> {
+    let text = fs::read_to_string(path).unwrap();
+    dbg!(&text);
+    text.split_terminator("\r\n")
+        .map(|line| line.split(" ").map(|tile| tile.parse().unwrap()).collect())
+        .collect()
+}
+
+fn load_textures() -> Vec<Texture> {
+    let texture_list = fs::read_to_string("TextureList.txt").unwrap();
+    dbg!(&texture_list);
+    // TODO handle both CRLF and LF properly
+    texture_list
+        .split_terminator("\r\n")
+        .map(|line| {
+            dbg!(line);
+            let mut parts = line.split(" ");
+            let name = parts.next().unwrap();
+            let a = parts.next().unwrap().parse().unwrap();
+            let b = parts.next().unwrap().parse().unwrap();
+            let c = parts.next().unwrap().parse().unwrap();
+            Texture::new(name.to_owned(), a, b, c)
+        })
+        .collect()
 }
