645dd7ae9151e71d82a6f223d67e7183eed34250
Martin Taibr <taibr.martin@gmail.com>
2020-08-03 00:55:47 +0200

    kill dead code

--- src/entities.rs
+++ src/entities.rs
@@ -6,8 +6,6 @@ use crate::data::{Map, Vec2f};
 pub struct GuidedMissile {
     pub pos: Vec2f,
     pub vel: Vec2f,
-    // pub dir: Vec2f,
-    // pub speed: f64,
     pub turn_rate: f64,
 }
 
@@ -17,7 +15,6 @@ pub fn spawn_guided_missile(rng: &mut SmallRng, map: &Map) -> GuidedMissile {
     // pos: Vec2f::new(640.0, 640.0),
     // vel: Vec2f::new(0.3, 0.2),
 
-    //let r: usize = random();
     let r = rng.gen_range(0, map.spawns().len());
     let spawn_index = map.spawns()[r];
     let spawn_pos = map.tile_center(spawn_index);
@@ -26,8 +23,6 @@ pub fn spawn_guided_missile(rng: &mut SmallRng, map: &Map) -> GuidedMissile {
     GuidedMissile {
         pos: spawn_pos,
         vel: Vec2f::new(1.0, 0.0).rotated_z(spawn_angle),
-        // dir: Vec2f::new(0.3, 0.2),
-        // speed: 50.0,
         turn_rate: 0.0,
     }
 }
