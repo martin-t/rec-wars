f3984730815972744d384e8b45e292efcefe1e88
Martin Taibr <taibr.martin@gmail.com>
2020-08-05 20:36:13 +0200

    better logging

--- src/logging.rs
+++ src/logging.rs
@@ -1,7 +1,31 @@
-/// A macro to provide `println!(..)`-style syntax for `console.log` logging.
+/// A macro to provide `println!(..)`-style syntax for `console.log` logging
 #[macro_export]
 macro_rules! log {
     ( $( $t:tt )* ) => {
         web_sys::console::log_1(&format!( $( $t )* ).into());
+    };
+}
+
+/// Print variables into console formatted as `var1: value1, val2: value2`
+#[macro_export]
+macro_rules! logd {
+    ( $( $t:tt ),* ) => {
+        let s = __print_pairs!( $( $t ),* );
+        web_sys::console::log_1(&s.into());
+    };
+}
+
+/// Helper to print the name and value of each given variable
+#[macro_export]
+macro_rules! __print_pairs {
+    ( $t:tt ) => {
+        format!("{}: {:.3}", stringify!($t), $t)
+    };
+    ( $t:tt, $( $rest:tt ),+ ) => {
+        format!(
+            "{}, {}",
+            __print_pairs!($t),
+            __print_pairs!( $( $rest ),+ )
+        )
     };
 }
