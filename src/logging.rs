57c33eff50d4c7d2ee5e0ecd3f769a9e1d442344
Martin Taibr <taibr.martin@gmail.com>
2020-08-05 20:36:37 +0200

    explicit separator

--- src/logging.rs
+++ src/logging.rs
@@ -1,8 +1,8 @@
 /// A macro to provide `println!(..)`-style syntax for `console.log` logging
 #[macro_export]
 macro_rules! log {
-    ( $( $t:tt )* ) => {
-        web_sys::console::log_1(&format!( $( $t )* ).into());
+    ( $( $t:tt ),* ) => {
+        web_sys::console::log_1(&format!( $( $t ),* ).into());
     };
 }
 
