3276ec261ceda96ad8141a987da52864d485b50c
Martin Taibr <taibr.martin@gmail.com>
2020-08-08 22:37:52 +0200

    doc GS

--- src/game_state.rs
+++ src/game_state.rs
@@ -1,6 +1,8 @@
 use crate::data::Vec2f;
 use crate::entities::GuidedMissile;
 
+/// Everyting that changes during the game
+/// and might need to be taken back during frame interpolation / reconciliation.
 #[derive(Debug, Clone)]
 pub struct GameState {
     pub guided_missile: GuidedMissile,
