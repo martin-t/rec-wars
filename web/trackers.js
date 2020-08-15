dfbfc3df1b6582c15bb2cd22446b8d0550c0be43
Martin Taibr <taibr.martin@gmail.com>
2020-08-15 08:31:19 +0200

    only render view counters on gitlab pages

---
+++ web/trackers.js
@@ -0,0 +1,13 @@
+if (!location.hostname === "localhost") {
+    document.write(`
+        <a href="https://www.webfreecounter.com/" target="_blank">
+            <img src="https://www.webfreecounter.com/hit.php?id=gvuvfxfcd&nd=6&style=1" alt="Views">
+        </a>
+        <img src="https://hitcounter.pythonanywhere.com/count/tag.svg?url=https%3A%2F%2Fgithub.com%2Fmartin-t%2Frec-wars-views"
+            alt="Views">
+        <img src="https://views.whatilearened.today/views/github/martin-t/rec-wars-views.svg" alt="Views">
+        <img src="https://komarev.com/ghpvc/?username=rec-wars-views" alt="Views">
+        <img src="https://visitor-badge.vercel.app/page/rec-wars-views" alt="Views">
+        <img src="https://pv-badge.herokuapp.com/total.svg?repo_id=martin-t-rec-wars-views" alt="Views">
+    `);
+}
