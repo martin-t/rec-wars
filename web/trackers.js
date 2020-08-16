a4eb95861ef958794cf334c1887e4cbf90f4a116
Martin Taibr <taibr.martin@gmail.com>
2020-08-16 04:22:21 +0200

    proxy a tracker through GH

--- web/trackers.js
+++ web/trackers.js
@@ -6,7 +6,7 @@ if (location.hostname !== "localhost") {
         <img src="https://hitcounter.pythonanywhere.com/count/tag.svg?url=https%3A%2F%2Fgithub.com%2Fmartin-t%2Frec-wars-views"
             alt="Views">
         <img src="https://views.whatilearened.today/views/github/martin-t/rec-wars-views.svg" alt="Views">
-        <img src="https://komarev.com/ghpvc/?username=rec-wars-views" alt="Views">
+        <img src="https://camo.githubusercontent.com/22d89e67ead7fd156e89b505a468b56db901735a/68747470733a2f2f6b6f6d617265762e636f6d2f67687076632f3f757365726e616d653d7265632d776172732d7669657773" alt="Views" data-canonical-src="https://komarev.com/ghpvc/?username=rec-wars-views" style="max-width:100%;">
         <img src="https://visitor-badge.vercel.app/page/rec-wars-views" alt="Views">
         <img src="https://pv-badge.herokuapp.com/total.svg?repo_id=martin-t-rec-wars-views" alt="Views">
     `);
