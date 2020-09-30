0f1272c7f8260a676407487cdc766d91021f47c1
Martin Taibr <taibr.martin@gmail.com>
2020-09-30 06:09:51 +0200

    make bfg direct hit kill

--- src/systems.rs
+++ src/systems.rs
@@ -11,32 +11,62 @@
 //! - https://github.com/amethyst/legion/issues/199 - I'd have to to split Pos
 //!   into separate components for vehicles and projectiles to be able to do collision detection.
 
-use legion::{query::IntoQuery, World};
+use legion::{query::IntoQuery, Entity, World};
 
 use crate::{
-    components::Bfg, components::Pos, components::Vel, cvars::Cvars, game_state::Explosion,
-    game_state::GameState, map::Map,
+    components::{Bfg, Destroyed, Owner, Pos, Vel},
+    cvars::Cvars,
+    entities,
+    game_state::{Explosion, GameState},
+    map::Map,
 };
 
 pub(crate) fn bfg(cvars: &Cvars, world: &mut World, map: &Map, gs: &mut GameState) {
+    let vehicles = entities::all_vehicles(world);
     let mut to_remove = Vec::new();
+    let mut to_kill = Vec::new();
 
-    let mut query = <(legion::Entity, &Bfg, &mut Pos, &Vel)>::query();
-    for (&entity, _, pos, vel) in query.iter_mut(world) {
-        pos.0 += vel.0 * gs.dt;
+    let mut query = <(Entity, &Bfg, &mut Pos, &Vel, &Owner)>::query();
+    for (&proj_id, _, proj_pos, proj_vel, proj_owner) in query.iter_mut(world) {
+        proj_pos.0 += proj_vel.0 * gs.dt;
 
-        if map.collision(pos.0) {
+        if map.collision(proj_pos.0) {
             gs.explosions.push(Explosion::new(
-                pos.0,
+                proj_pos.0,
                 cvars.g_bfg_explosion_scale,
                 gs.frame_time,
                 true,
             ));
-            to_remove.push(entity);
+            to_remove.push(proj_id);
+        }
+
+        for &(veh_id, veh_destroyed, veh_pos, _veh_angle, _veh_hitbox) in &vehicles {
+            if !veh_destroyed.0
+                && veh_id != proj_owner.0
+                && (proj_pos.0 - veh_pos.0).magnitude_squared() <= 24.0 * 24.0
+            {
+                to_remove.push(proj_id);
+
+                // Projectile explosion above vehicle explosion because it looks better.
+                gs.explosions
+                    .push(Explosion::new(veh_pos.0, 1.0, gs.frame_time, false));
+                gs.explosions
+                    .push(Explosion::new(proj_pos.0, 1.0, gs.frame_time, true));
+
+                to_kill.push(veh_id);
+
+                break;
+            }
         }
     }
 
     for entity in to_remove {
         world.remove(entity);
+    }
+
+    for veh_id in to_kill {
+        let mut entry = world.entry(veh_id).unwrap();
+        let destroyed = entry.get_component_mut::<Destroyed>().unwrap();
+        destroyed.0 = true;
     }
 }
