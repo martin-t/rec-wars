678b51998089c8bdc4e2325721605ad161525f6c
Martin Taibr <taibr.martin@gmail.com>
2020-11-14 19:12:37 +0100

    add warn

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 
 // Additional warnings that are allow by default (`rustc -W help`)
 //#![warn(missing_copy_implementations)]
-//#![warn(missing_debug_implementations)]
+#![warn(missing_debug_implementations)]
 #![warn(trivial_casts)]
 #![warn(trivial_numeric_casts)]
 #![warn(unreachable_pub)]
