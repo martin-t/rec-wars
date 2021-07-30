c85e7f6c8a1352b4d0893ebe29e938e91ba785a8
Martin Taibr <taibr.martin@gmail.com>
2021-07-30 08:59:15 +0200

    Load a random map by default

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,8 @@ struct Opts {
     #[structopt(long)]
     splitscreen: bool,
 
-    #[structopt(long, default_value = "maps/Atrium.map")]
-    map: String,
+    #[structopt(long)]
+    map: Option<String>,
 
     #[structopt(long)]
     seed: Option<u64>,
@@ -53,10 +53,10 @@ async fn main() {
     if let Some(seed) = opts.seed {
         cvars.d_seed = seed;
     }
+    let now = std::time::SystemTime::now();
+    let unix_secs = now.duration_since(UNIX_EPOCH).unwrap().as_secs();
     if cvars.d_seed == 0 {
-        let now = std::time::SystemTime::now();
-        let secs = now.duration_since(UNIX_EPOCH).unwrap().as_secs();
-        cvars.d_seed = secs;
+        cvars.d_seed = unix_secs;
     }
 
     // LATER Load texture list and map in parallel with other assets
@@ -64,7 +64,70 @@ async fn main() {
     draw_text("Loading...", 400.0, 400.0, 32.0, PURPLE);
     let tex_list_text = str::from_utf8(&tex_list_bytes).unwrap();
     let surfaces = map::load_tex_list(tex_list_text);
-    let map_bytes = load_file(&opts.map).await.unwrap();
+
+    // This is a subset of maps that are not blatantly broken with the current bots.
+    let maps = [
+        //"Arena",
+        //"A simple plan (2)",
+        "Atrium",
+        "Bunkers (2)",
+        "Castle Islands (2)",
+        "Castle Islands (4)",
+        //"Corners (4)",
+        "Delta",
+        "Desert Eagle",
+        //"Joust (2)",
+        //"Large front (2)",
+        //"Oases (4)",
+        "Park",
+        "Roads",
+        "Snow",
+        "Spots (8)",
+        //"Vast Arena",
+        //"extra/6 terrains (2)",
+        //"extra/A Cow Too Far",
+        //"extra/All Water",
+        //"extra/Battlegrounds (2)",
+        //"extra/Crossing", // No spawns
+        "extra/Damned Rockets (2)", // Asymmetric CTF, left half like Castly Islands (2), right half has 2 bases
+        //"extra/doom",
+        //"extra/elements",
+        //"extra/Exile (4)", // Tiny, many spawns
+        //"extra/football",
+        "extra/Ice ring",
+        //"extra/ice skating ring (2)",
+        "extra/IceWorld",
+        "extra/I see you (2)", // Like Large Front (2) but without any cover
+        //"extra/Knifflig (2)",
+        //"extra/Large",
+        //"extra/Neutral",
+        "extra/Nile",
+        //"extra/OK Corral (2)", // Not symmetric (upper spawn is closer)
+        //"extra/Peninsulae (3)",
+        //"extra/River Crossings",
+        //"extra/Road To Hell (2)", // Only 4 spawns in a tiny area
+        //"extra/THE Crossing",
+        //"extra/Thomap1 (4)",
+        //"extra/Town on Fire",
+        "extra/twisted (2)",
+        //"extra/winterhardcore",
+        "extra/Yellow and Green",
+        "extra2/Mini Islands (4)",
+        //"extra2/Symmetric",
+        //"extra2/Training room",
+        //"extra2/Winter (4)",
+        //"extra2/World War (2)",
+    ];
+    let mut map_path = opts
+        .map
+        .unwrap_or_else(|| maps[unix_secs as usize % maps.len()].to_owned());
+    if !map_path.ends_with(".map") {
+        map_path.push_str(".map");
+    }
+    if !map_path.starts_with("maps/") {
+        map_path.insert_str(0, "maps/");
+    }
+    let map_bytes = load_file(&map_path).await.unwrap();
     draw_text("Loading...", 400.0, 400.0, 32.0, PURPLE);
     let map_text = str::from_utf8(&map_bytes).unwrap();
     let map = map::load_map(&map_text, surfaces);
