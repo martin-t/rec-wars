745673c798e85ed76bbc4f81a1dd8143409e9bc6
Martin Taibr <taibr.martin@gmail.com>
2021-08-02 10:38:29 +0200

    Cvars: test more fields

--- cvars-derive/tests/parse.rs
+++ cvars-derive/tests/parse.rs
@@ -1,8 +1,19 @@
 use cvars_derive::Cvars;
 
-#[derive(Cvars)]
+#[derive(Debug, Clone, Cvars)]
 struct Cvars {
-    pub g_idk: i32,
+    pub g_bool: bool,
+    pub g_int: i32,
+    pub g_usize: usize,
+    pub g_float: f32,
+    pub g_double: f64,
+    pub g_enum: Enum,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Enum {
+    One,
+    Two,
 }
 
 fn main() {}
