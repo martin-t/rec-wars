11e1ac634300d712f715d9cc5e6fe6cce2858a92
Martin Taibr <taibr.martin@gmail.com>
2021-08-02 12:34:01 +0200

    List alternatives

--- cvars-derive/src/lib.rs
+++ cvars-derive/src/lib.rs
@@ -2,6 +2,12 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
+// TODO alternatives:
+//  https://crates.io/crates/tuna
+//  https://crates.io/crates/cvar
+//  https://crates.io/crates/const-tweaker
+//  https://crates.io/crates/inline_tweak
+
 #[proc_macro_derive(Cvars)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let _input = parse_macro_input!(input as DeriveInput);
